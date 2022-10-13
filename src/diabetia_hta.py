7842443ee9188ea00290bf5f7e6930f28a480566
Aide Jazmín González Cruz <aide.jgc@gmail.com>
2022-10-13 17:54:44 -0500

    Agregando main para entrenar o predecir datos

--- src/diabetia_hta.py
+++ src/diabetia_hta.py
@@ -11,7 +11,7 @@ from pipeline import ingestion, transformation, feature_engineering, modeling
 # python diabetia_hta.py 2 "aide.csv"
 
 # Argumentos
-# 1 entrenamiento o predicción 
+# 1 entrenamiento o predicci�n 
 # archivo de datos
 # ventana
 
@@ -19,37 +19,30 @@ ent_pred = sys.argv[1]
 file_data = sys.argv[2]
 
 
-def main ():
-    #print("ent_pred: ",type(ent_pred))
-    #print("file_data :", file_data)
+def main():
     # Entrenamiento
     if (ent_pred == "1"):
         print("Iniciando entrenamiento--------------------------------------------------------------")
-        #df = ingestion.ingesta_data(file_data, "../Data/data.pkl")
-        #df = transformation.transform(df, "../Data/transformation.pkl")
-        #df = feature_engineering.feature_engineering(df, "../Data/feature_eng.pkl")
-        #df_m = df.loc[:, df.columns != 'cx_curp']
+        df = ingestion.ingesta_data(file_data, "../Data/data.pkl")
+        df = transformation.transform(df, "../Data/transformation.pkl")
+        df = feature_engineering.feature_engineering(df, "../Data/feature_eng.pkl")
+        df_m = df.loc[:, df.columns != 'cx_curp']
         #df_curp = df[["cx_curp"]]
-        #model_and_features, X_test, y_test = modeling.training(df_m)
-        #model = modeling.best_model(model_and_features, X_test, y_test, "../Data/best_model.pkl")
+        model_and_features, X_test, y_test = modeling.training(df_m)
+        model = modeling.best_model(model_and_features, X_test, y_test, "../Data/best_model.pkl")
         print("Finaliza entrenamiento ---------------------------------------------------------------")
         
-    # Predicción
+    # Predicci�n
     elif (ent_pred == "2"):
-        print("Comenzando predicción ----------------------------------------------------------------")
-        #df = ingestion.ingesta_data(file_data, "../Data/data_new.pkl")
-        #df = transformation.transform(df, "../Data/transformation_new.pkl")
-        ## dejar en ventanas
-        #df = feature_engineering.feature_engineering(df, "../Data/feature_eng_new.pkl")
-        #df_m = df.loc[:, df.columns != 'cx_curp']
-        #df_curp = df[["cx_curp"]]
-        #modeling.predict()
-        
+        print("Comenzando predicci�n ----------------------------------------------------------------")
+        df = ingestion.ingesta_data(file_data, "../Data/new_data.pkl")
+        df['hba1c'] = df['hba1c'].astype(str)
+        df = transformation.transform(df, "../Data/new_transformation.pkl")
+        df = feature_engineering.feature_engineering(df, "../Data/new_data_fe.pkl")
+        df = modeling.predict(df, "../Data/best_model.pkl", "../Data/save_new_predict.pkl")
+        print("Fin predicci�n ----------------------------------------------------------------")
     else:
         print("Argumentos incorrectos")
-        
-    print("FIN")
-    
     pass
 
 

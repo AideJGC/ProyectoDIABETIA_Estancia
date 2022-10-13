e9a2f120c36ca03ecf4d4d9dd7a1f8f941499a1b
Aide Jazmín González Cruz <aide.jgc@gmail.com>
2022-10-12 21:47:16 -0500

    Agregando path para salvar ingesta

--- src/pipeline/ingestion.py
+++ src/pipeline/ingestion.py
@@ -5,6 +5,7 @@ from os.path import dirname
 from src.utils import utils
 sys.path.append(dirname('../src'))
 
-def ingesta_data(path):
+def ingesta_data(path, path_save):
     df = pd.read_csv(path)
-    utils.save_df(df, "../Data/data.pkl")
+    utils.save_df(df, path_save)
+    return df

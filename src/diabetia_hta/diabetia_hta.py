982866ea67a2ad9d1c19b895299bc8b42801f87d
Aide Jazmín González Cruz <aide.jgc@gmail.com>
2022-10-15 13:52:51 -0500

    Actualizando path librerias y carpeta data

--- src/diabetia_hta/diabetia_hta.py
+++ src/diabetia_hta/diabetia_hta.py
@@ -5,7 +5,7 @@ from os.path import dirname
 import warnings
 warnings.filterwarnings('ignore')
 sys.path.append(dirname('../src'))
-from pipeline import ingestion, transformation, feature_engineering, modeling
+from diabetia_hta.pipeline import ingestion, transformation, feature_engineering, modeling
 
 # RUN
 # python diabetia_hta.py 1 "aide.csv"
@@ -25,13 +25,13 @@ def main():
     if (ent_pred == "1"):
         print("Iniciando entrenamiento---------------------------------------------------------------")
         start_time = time.time()
-        df = ingestion.ingesta_data(file_data, "../Data/data.pkl")
-        df = transformation.transform(df, "../Data/transformation.pkl")
-        df = feature_engineering.feature_engineering(df, "../Data/feature_eng.pkl")
+        df = ingestion.ingesta_data(file_data, "../data/data.pkl")
+        df = transformation.transform(df, "../data/transformation.pkl")
+        df = feature_engineering.feature_engineering(df, "../data/feature_eng.pkl")
         df_m = df.loc[:, df.columns != 'cx_curp']
         #df_curp = df[["cx_curp"]]
         model_and_features, X_test, y_test = modeling.training(df_m)
-        model = modeling.best_model(model_and_features, X_test, y_test, "../Data/best_model.pkl")
+        model = modeling.best_model(model_and_features, X_test, y_test, "../data/best_model.pkl")
         print("Finaliza entrenamiento en ", time.time() - start_time, " segundos")
         print("--------------------------------------------------------------------------------------")
         
@@ -39,11 +39,11 @@ def main():
     elif (ent_pred == "2"):
         print("Comenzando predicci�n ----------------------------------------------------------------")
         start_time = time.time()
-        df = ingestion.ingesta_data(file_data, "../Data/new_data.pkl")
+        df = ingestion.ingesta_data(file_data, "../data/new_data.pkl")
         df['hba1c'] = df['hba1c'].astype(str)
-        df = transformation.transform(df, "../Data/new_transformation.pkl")
-        df = feature_engineering.feature_engineering(df, "../Data/new_data_fe.pkl")
-        df = modeling.predict(df, "../Data/best_model.pkl", "../Data/save_new_predict.pkl")
+        df = transformation.transform(df, "../data/new_transformation.pkl")
+        df = feature_engineering.feature_engineering(df, "../data/new_data_fe.pkl")
+        df = modeling.predict(df, "../data/best_model.pkl", "../data/save_new_predict.pkl")
         print("Fin predicci�n en ", time.time() - start_time, " segundos")
         print("--------------------------------------------------------------------------------------")
     else:

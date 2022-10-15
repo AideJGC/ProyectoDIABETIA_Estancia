848e98df9c8fc7c1a29a2d632a4b8e492fa3e8b9
Aide Jazmín González Cruz <aide.jgc@gmail.com>
2022-10-15 17:36:46 -0500

    Cambiando path libraries

--- src/diabetia_hta/pipeline/ingestion.py
+++ src/diabetia_hta/pipeline/ingestion.py
@@ -2,9 +2,9 @@ import pandas as pd
 import sys
 import os
 from os.path import dirname
+sys.path.append(dirname('../src'))
 from datetime import datetime
 from src.utils import utils
-sys.path.append(dirname('../src'))
 
 def ingesta_data(path, path_save):
     df = pd.read_csv(path)

52d25e090c6d1e56a3eadd1b3aec9e9028fbdbb1
AIDE JAZMIN GONZALEZ CRUZ <69474196+AideJGC@users.noreply.github.com>
2022-10-14 23:22:35 -0500

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="diabetia_hta",
+    version="0.0.1",
+    author="Aide Jazmín González Cruz",
+    description="Aplicación para predecir que pacientes diabeticos tendrán hipertensión el siguiente año o el próximo mes.",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7.3",
+)
